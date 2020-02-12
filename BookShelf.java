d80dbd9e20bc84f0c685eda2b10eb1e453108314
宇都宮靖之 <utsunomiyayasuyuki@utsunomiyayasuyukinoMacBook-Pro.local>
2020-02-12 06:59:28 -0800

    second_commit

--- BookShelf.java
+++ BookShelf.java
@@ -1,5 +1,11 @@
 public class BookShelf implements Aggregate{
     private Book[] books;
     private int last = 0;
-    
+    public BookShelf(int macsize){
+        this.books = books;
+    }
+
+    public Book getBook(int index){
+        return books[index];
+    }
 }
